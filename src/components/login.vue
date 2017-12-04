67443782f9527775c0d772e4f7fc6a7f38aa64c2
yhn <2461379674@qq.com>
2017-12-04 18:12:08 +0800

    yhn 12.4

--- src/components/login.vue
+++ src/components/login.vue
@@ -12,40 +12,71 @@
 
 <script>
 import apifuns from "../api";
+import axios from "axios";
+
+// function getsongList() {
+//   let data={
+//     listId:"list1"
+//   }
+//   axios({
+//     method: "post",
+//     url: "yhntest/getplaylistsongs",
+//     data: data
+//   })
+//     .then(function(response) {
+//       console.log(response.data);
+//     })
+//     .catch(function(error) {
+//     });
+// }
 
 export default {
   name: "login",
   data() {
     return {
-      uname:"",
-      pwd:""
+      uname: "",
+      pwd: ""
     };
   },
   methods: {
-    login(){
-      let data={};
-      data.uname=this.uname;
-      data.pwd=this.pwd;
-       apifuns.login(data).then(bcData=>{
-         console.log(bcData);
-         if(bcData.status===true){
-           alert(bcData.info)
-           this.$router.replace('/home');
-         }
-       });
+    login() {
+      let data = {};
+      data.uname = this.uname;
+      data.pwd = this.pwd;
+      apifuns.login(data).then(bcData => {
+        console.log(bcData);
+        if (bcData.status === true) {
+          alert(bcData.info);
+          this.$router.replace("/home");
+        }
+      });
     },
-    goRegister(){
+    goRegister() {
       this.$router.push("/register");
     }
   },
-  created () {
+  async created() {
+    let data={
+    listId:"list2"
+  }
+ let obj=await axios({
+    method: "post",
+    url: "yhntest/getplaylistsongs",
+    data: data
+  })
+    .then(function(response) {
+      console.log(response.data);
+      return response.data
+    })
+    .catch(function(error) {
+    });
 
   },
-  mounted: function() {
-  }
+  mounted: function() {}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
